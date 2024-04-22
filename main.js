7e93209043d6127b0299b8cdd18de23a9d1ecaba
gustavo gabriel fernandes pedrozo <107936389+gustavogabrielfernandes@users.noreply.github.com>
2024-04-22 09:21:32 -0300

    main.js01

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-const botoes = documen.queryselectorALL(".botao");
-const textos = documen.queryselectorALL(".aba-conteudo");
+const botoes = documen.querySelectorALL(".botao");
+const textos = documen.querySelectorALL(".aba-conteudo");
+
+for(let i = 0;i <botoes.length; i++){
+    botoes[i].onclick = function (){
+
+        for (let j = 0; j <botoes.length;j++) {
+    botoes[j]classList.remove("ativo");
+    textos[j]classList.remove("ativo");
+     }
+
+     botoes[i].classList.add("ativo");
+     textos[]i.classList.add("ativo");
+    }
+}
+
+const contadores = ducument.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
